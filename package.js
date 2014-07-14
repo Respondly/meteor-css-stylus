e08b2a1078a407a8a86d3a30bea496c404ab65f8
Phil Cockfield <phil@cockfield.net>
2014-07-13 19:07:12 -0700

    Initial commit

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  summary: 'Meteor Stylus package extended to support Mixins.'
+});
+
+
+Package._transitional_registerBuildPlugin({
+  name: "compileStylus",
+  use: [],
+  sources: [
+    'plugin/compile-stylus.js'
+  ],
+  npmDependencies: { stylus: "0.47.1", nib: "1.0.3" }
+});
+
+
+
+
+Package.on_test(function (api) {
+  api.use(['munit', 'coffeescript', 'chai', 'test-helpers', 'templating']);
+  api.use('stylus-compiler');
+
+  // Generated with: github.com/philcockfield/meteor-package-loader
+  api.add_files('tests/client/sample/sample.html', 'client');
+  api.add_files('tests/client/sample/sample.import.styl', 'client');
+  api.add_files('tests/client/sample/sample.styl', 'client');
+  api.add_files('tests/client/base-tests.coffee', 'client');
+
+});
